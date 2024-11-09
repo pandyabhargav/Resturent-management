bd04f651c5f5e147e99bed7f5358cb386f83799b
pandyabhargav <137371479+pandyabhargav@users.noreply.github.com>
2024-11-09 14:25:14 +0530

    comit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Registration from './pages/signup/signup';
+import Login from './pages/login/login';
 
 function App() {
   
 
   return (
     <>
-     
+      <Router>
+      <div className="App">
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
     </>
   );
 }
